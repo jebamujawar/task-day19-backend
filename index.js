5ed515ddfc09914c13439eb1df70104940a75406
Jeba Bashir Mujawar <jebamujawar351@gmail.com>
2026-01-04 08:11:38 +0530

    updated index file

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const tasksRoute = require("./routes/tasks");
 
-const app = express();
+const app = express(); // Define app first
+
+// Middleware
+app.use(cors());
 app.use(express.json()); // parse JSON
 
 // MongoDB connection
-mongoose.connect("mongodb://localhost:27017/taskDB")
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.log(err));
+mongoose.connect(process.env.MONGO_URL || "mongodb://localhost:27017/taskDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log(err));
 
 // Routes
 app.use("/tasks", tasksRoute);
@@ -18,6 +25,4 @@ app.get("/", (req, res) => res.send("Server working"));
 
 // Start server
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
